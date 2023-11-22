bf42470e4518e3e29514ffbe249a1301164d3dc9
rapha <raphaelmrodrigues@hotmail.com>
2023-11-22 18:56:49 -0300

    third commit

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Usuario
+from django.shortcuts import redirect
 # Create your views here.
 
 def login(request):
@@ -7,3 +9,25 @@ def login(request):
 
 def cadastro(request):
     return render(request, 'cadastro.html')
+
+def valida_cadastro(request):
+    nome = request.POST.get('nome')
+    senha = request.POST.get('senha')
+    email = request.POST.get('email')
+
+    if len(nome.strip()) == 0 or len(email.strip()) == 0:
+        return redirect('/auth/cadastro/?status=1')
+    if len(senha) < 8:
+        return redirect('/auth/cadastro/?status=2')
+
+    usuario = Usuario.objects.filter(email=email)
+    if len(usuario) > 0:
+        return redirect('/auth/cadastro/?status=3')
+    try:
+        usuario = Usuario(nome =nome, senha = senha, email = email)
+        usuario.save()
+        return redirect('/auth/cadastro/?status=0')
+    except:
+        return redirect('/auth/cadastro/?status=4')
+
+    return HttpResponse(f"{nome}{senha}{email}")
