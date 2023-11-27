2449a4a0e6d8c315219a754fef583959cd95224b
rapha <raphaelmrodrigues@hotmail.com>
2023-11-27 00:51:41 -0300

    third commit

--- livro/urls.py
+++ livro/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home')
-
+    path('home/', views.home, name='home'),
+    path('ver_livro/<int:id>', views.ver_livros, name= 'ver_livros')
 ]
--- livro/views.py
+++ livro/views.py
@@ -14,3 +14,12 @@ def home(request):
         return render(request, 'home.html', {'livros': livros})
     else:
         return redirect('/auth/login/?status=2')
+
+def ver_livros(request, id):
+    if request.session.get('usuario'):
+        livros = Livros.objects.get(id = id)
+        if request.session.get('usuario') == livros.usuario.id:
+            return render(request, 'ver_livro.html', {'livro': livros})
+        else:
+            return HttpResponse('Esse livro não é seu')
+    return redirect('/auth/login/?status=2')
