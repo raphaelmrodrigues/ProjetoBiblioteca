d2c4cb1a60942469420cd8df5e2d4ef17157f27f
rapha <raphaelmrodrigues@hotmail.com>
2023-11-27 21:57:48 -0300

    third commit

--- livro/models.py
+++ livro/models.py
@@ -17,13 +17,20 @@ class Livros(models.Model):
     autor = models.CharField(max_length = 30)
     co_autor = models.CharField(max_length = 30, blank=True)
     data_cadastro = models.DateField(default=date.today())
+    i = (
+        ("PT-BR", "Portugues-BR"),
+        ("EN-US", "Ingles (Estados Unidos)"),
+        ("FR", "Frances"),
+        ("ES", "Espanhol"),
+    )
+    idioma = models.CharField(max_length=30, choices=i, default="PT=BR")
     emprestado = models.BooleanField(default=False)
     nome_emprestado = models.CharField(max_length=30, blank=True, null=True)
     data_emprestimo = models.DateTimeField(blank=True, null=True)
     data_devolucao = models.DateTimeField(blank=True, null=True)
     tempo_duracao = models.DateField(blank=True, null=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
-    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING, blank=True, null=True)
 
 
     class Meta:
--- livro/views.py
+++ livro/views.py
@@ -9,8 +9,7 @@ from livro.models import Livros
 
 def home(request):
     if request.session.get('usuario'):
-        usuario = Usuario.objects.get(id = request.session['usuario'])
-        livros = Livros.objects.filter(usuario = usuario)
+        livros = Livros.objects.all()
         return render(request, 'home.html', {'livros': livros})
     else:
         return redirect('/auth/login/?status=2')
