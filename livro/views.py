b2412d4bdc7a28b3c90c42eb77a81a421b6079cb
rapha <raphaelmrodrigues@hotmail.com>
2023-12-13 19:01:51 -0300

    third commit

--- livro/views.py
+++ livro/views.py
@@ -10,7 +10,7 @@ from livro.models import Livros, Emprestimos
 def home(request):
     if request.session.get('usuario'):
         livros = Livros.objects.all()
-        return render(request, 'home.html', {'livros': livros})
+        return render(request, 'home.html', {'livros': livros, 'usuario_logado': request.session.get('usuario')})
     else:
         return redirect('/auth/login/?status=2')
 
@@ -18,5 +18,5 @@ def ver_livros(request, id):
     if request.session.get('usuario'):
         livros = Livros.objects.get(id = id)
         emprestimos = Emprestimos.objects.filter(livro = livros)
-        return render(request, 'ver_livro.html', {'livro': livros, 'emprestimos': emprestimos})
+        return render(request, 'ver_livro.html', {'livro': livros, 'emprestimos': emprestimos, 'usuario_logado': request.session.get('usuario')})
 
