41ef8acc8d3e8852edb05c5f90152acf4ca854dc
rapha <raphaelmrodrigues@hotmail.com>
2023-12-14 19:40:19 -0300

    third commit

--- livro/views.py
+++ livro/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from usuarios.models import Usuario
 from livro.models import Livros, Emprestimos
+from .forms import CadastroLivro
 
 
 # Create your views here.
@@ -10,13 +11,25 @@ from livro.models import Livros, Emprestimos
 def home(request):
     if request.session.get('usuario'):
         livros = Livros.objects.all()
-        return render(request, 'home.html', {'livros': livros, 'usuario_logado': request.session.get('usuario')})
+        form = CadastroLivro()
+
+        return render(request, 'home.html', {'livros': livros, 'usuario_logado': request.session.get('usuario'), 'form': form})
     else:
         return redirect('/auth/login/?status=2')
 
 def ver_livros(request, id):
     if request.session.get('usuario'):
         livros = Livros.objects.get(id = id)
+        form = CadastroLivro()
         emprestimos = Emprestimos.objects.filter(livro = livros)
-        return render(request, 'ver_livro.html', {'livro': livros, 'emprestimos': emprestimos, 'usuario_logado': request.session.get('usuario')})
+        return render(request, 'ver_livro.html', {'livro': livros, 'emprestimos': emprestimos, 'usuario_logado': request.session.get('usuario'), 'form': form})
 
+def cadastrar_livro(request):
+    if request.method == 'POST':
+        form = CadastroLivro(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponse
+        else:
+            return HttpResponse('DADOS INVALIDOS')
